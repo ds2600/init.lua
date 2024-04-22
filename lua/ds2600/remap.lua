682452ec92826cfd951cc774805d29907627f730
ds2600 <william.bunce@outlook.com>
2024-04-22 11:31:32 -0400

    Update remap.lua

--- lua/ds2600/remap.lua
+++ lua/ds2600/remap.lua
@@ -13,9 +13,17 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+-- Map <leader>pnf to create new file in netrw
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "netrw",
     callback = function()
-        vim.api.nvim_buf_set_keymap(0, 'n', '<leader>pnf', ':let g:netrw_banner=0<CR>:call netrw#NetrwBrowseX(expand("%:p"),0)<CR>', {noremap = true, silent = true})
+        vim.api.nvim_buf_set_keymap(0, 'n', '<leader>pnf', ':lua CreateNewFileInNetrw()<CR>', {noremap = true, silent = true})
     end
 })
+
+function CreateNewFileInNetrw()
+    local filename = vim.fn.input('New File Name: ')
+    if filename ~= '' then
+        vim.cmd('edit ' .. vim.fn.expand('%:p:h') .. '/' .. filename)
+    end
+end
