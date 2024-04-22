a89132c4d02af981b0b48d1005d59c77d5b2af91
ds2600 <william.bunce@outlook.com>
2024-04-22 11:17:40 -0400

    Update remap.lua
    Added new file in netrw

--- lua/ds2600/remap.lua
+++ lua/ds2600/remap.lua
@@ -12,3 +12,10 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "netrw",
+    callback = function()
+        vim.api.nvim_buf_set_keymap(0, 'n', '<leader>pnf', ':let g:netrw_banner=0<CR>:call netrw#NetrwBrowseX(expand("%:p"),0)<CR>', {noremap = true, silent = true})
+    end
+})
